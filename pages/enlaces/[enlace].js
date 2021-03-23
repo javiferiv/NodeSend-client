20b8fd73a3b7ad56d846a64e8d9ebbd65543ef04
Javi Fernandez <javierfernandezriv@gmail.com>
2021-03-23 14:29:44 +0100

    Validating password

--- pages/enlaces/[enlace].js
+++ pages/enlaces/[enlace].js
@@ -1,5 +1,8 @@
+import React, { useState, useContext } from 'react';
 import Layout from './../../components/Layout';
+import Alerta from './../../components/Alerta';
 import clienteAxios from './../../config/axios';
+import appContext from './../../context/app/appContext';
 
 export async function getServerSideProps({ params }) {
     const { enlace } = params;
@@ -24,15 +27,77 @@ export async function getServerSidePaths() {
 
 const Enlaces = ({ enlace }) => {
 
+    //Context de la app
+    const AppContext = useContext(appContext);
+    const { mostrarAlerta, mensajeArchivo } = AppContext;
+
+    const [tienePassword, setTienePassword] = useState(enlace.password);
+    const [password, setPassword] = useState('');
+
+    const verificarPassword = async e => {
+        e.preventDefault();
+
+        const data = {
+            password
+        };
+
+        try {
+            const resultado = await clienteAxios.post(`/api/enlaces/${enlace.enlace}`, data)
+            setTienePassword(resultado.data.tienePassword)
+        }
+        catch (error) {
+            mostrarAlerta(error.response.data.msg)
+        }
+    }
+
     return (
         <Layout>
-            <h1 className="text-4xl text-center text-gray-700">Descarga tu archivo: </h1>
-            <div className="flex items-center justify-center mt-10">
-                <a
-                    href={`${process.env.backendURL}/api/archivos/${enlace.archivo}`}
-                    className="bg-red-500 text-center px-10 py-3 rounded uppercase font-bold text-white cursor-pointer"
-                >Aquí</a>
-            </div>
+            { tienePassword ? (
+                <>
+                    <p className="text-center">Este enlace está protegido por un password. Introdúcelo a continuación</p>
+                    {mensajeArchivo && <Alerta />}
+                    <div className="flex justify-center mt-5">
+                    <div className="w-full max-w-lg">
+                        <form
+                            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                            onSubmit={e => verificarPassword(e)}
+                        >       
+                            <div className="mb-4">
+                                <label
+                                    className="block text-black text-sm font-bold mb-2"
+                                    htmlFor="password"
+                                >Password</label>
+                                <input
+                                    type="password"
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="password"
+                                    placeholder="Password del enlace"
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                />
+                            </div>
+                            <input
+                                type="submit"
+                                className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold cursor-pointer"
+                                value="Validar Password"
+                                />
+                        </form>
+                    </div>
+                    </div>
+                </>
+            )
+                : (
+                <>
+                <h1 className="text-4xl text-center text-gray-700">Descarga tu archivo: </h1>
+                    <div className="flex items-center justify-center mt-10">
+                        <a
+                            href={`${process.env.backendURL}/api/archivos/${enlace.archivo}`}
+                            className="bg-red-500 text-center px-10 py-3 rounded uppercase font-bold text-white cursor-pointer"
+                        >Aquí</a>
+                    </div>
+                </>
+
+            )}
         </Layout>
      );
 }
